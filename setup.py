a16264e31e6222ef60311959e4d046d7366ba261
David Turner <novalis@novalis.org>
2007-06-19 16:27:58 +0000

    added info
    git-svn-id: https://svn.socialplanning.org/svn/CabochonClient/trunk@6484 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='CabochonClient',
-    version="",
-    #description="",
+    version="0.1",
+    description="A client for the Cabochon event server",
     author="David Turner",
     author_email="novalis@openplans.org",
-    #url="",
+    url="http://www.openplans.org/projects/cabochon",
+    license="GPLv2 or any later version",
     install_requires=["simplejson"],
     packages=find_packages(),
     include_package_data=True,
